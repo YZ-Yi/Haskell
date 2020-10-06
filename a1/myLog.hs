6277c5180618b12e8ae07e12bfe0831aa6aa4509
Yuzhe <yiyuzhe0125@outlook.com>
2020-10-05 22:50:13 -0600

    finished finally edition of a1

--- a1/myLog.hs
+++ a1/myLog.hs
@@ -4,3 +4,12 @@ myLog :: Integer -> Integer -> Integer
 myLog b x
     | b > x     = 0
     | b <= x    = 1 + myLog b (quot x b)
+
+loga_b :: Integer -> Integer -> Int
+loga_b a b
+	| b < a 	= 0
+	| b >= a 	= 1 + loga_b a (b `div` a)
+
+test :: Int
+test = loga_b 2 16
+
--- a1/scalePic.hs
+++ a1/scalePic.hs
@@ -8,32 +8,60 @@ scale   pic size
 
 
 -- other functions
-repeatPix   :: Char -> Int -> [Char]
-repeatPix   c s
-    | s == 0    = []
-    | s > 0     = c : (repeatPix c (s - 1))
-
 scalePix    :: [Char] -> Int -> [Char]
 scalePix     str s
-    | str == [] = []
-    | otherwise = (repeatPix (head str) s) ++ (scalePix (tail str) s)
+    = concat (map (replicate s) str)
 
 scaleLine   :: Picture -> Int -> Picture
 scaleLine   pic size
     = [scalePix ln size | ln <- pic]
 
-repeatStr   :: [Char] -> Int -> [[Char]]
-repeatStr   str size
-    | size == 0 = []
-    | size > 0  = str : (repeatStr str (size - 1))
 
 scaleStr    :: Picture -> Int -> Picture
 scaleStr    pic size
-    | pic == [] = []
-    | otherwise = (repeatStr (head pic) size) ++ (scaleStr (tail pic) size)
+    = concat (map (replicate size) pic)
 
 -- test 
 examplePic  :: Picture
 examplePic  = ["#.#", "..#"]
 test    :: Picture
 test    = scale examplePic 2
+
+--purpose: repeat one pixel certain times
+--parameter: c: the input pixel; s: the times we want to scale the pixel
+--return value: a list of pixel after repeating the input pixel
+repeatPix   :: Char -> Int -> [Char]
+repeatPix   c s
+    | s == 0    = []
+    | s > 0     = c : (repeatPix c (s - 1))
+
+--purpose: scale one row of pixels
+--parameter: str: the input pixels; s: the times we want to scale the pixels
+--return value: a new row of pixels after scaling the input pixels
+scalePix    :: [Char] -> Int -> [Char]
+scalePix     str s
+    | str == [] = []
+    | otherwise = (repeatPix (head str) s) ++ (scalePix (tail str) s)
+
+--purpose: scale the picture horizontally
+--parameter: pic: the input picture; size: the times we want to scale the picture
+--return value: the new picture after scaling the input picture horizontally
+scaleLine   :: Picture -> Int -> Picture
+scaleLine   pic size
+    = [scalePix ln size | ln <- pic]
+
+--purpose: repeat one row of pixels certain times
+--parameter: str: the input row of pixels; size: the times we want to repeate the pixels
+--return value: a picture after repeating the input row of pixels
+repeatStr   :: [Char] -> Int -> [[Char]]
+repeatStr   str size
+    | size == 0 = []
+    | size > 0  = str : (repeatStr str (size - 1))
+
+--purpose: scale the picture vertically
+--parameter: pic: the input picture; size: the times we want to scale the picture
+--return value: a new picture after we scale the input picture vertically
+scaleStr    :: Picture -> Int -> Picture
+scaleStr    pic size
+    | pic == [] = []
+    | otherwise = (repeatStr (head pic) size) ++ (scaleStr (tail pic) size)
