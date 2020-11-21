b04ce91e82911ed11ac898b2f76f3d85fea5642f
Yuzhe <yiyuzhe0125@outlook.com>
2020-11-20 17:13:38 -0700

    some changes

--- a4/q4.hs
+++
@@ -1,6 +0,0 @@
-runningSum :: [Int] -> [Int]
-runningSum xs = 0 : (func xs 0)
-
--------------------helper function------------------------
-func :: [Int] -> Int -> [Int]
-func (x:xs) y = (x + y):(func xs (x + y))
